a41f0e3e5dc70a3f5d2560d062e7f9ad56332258
Muhammad Tajjiou <shutsugan@gmail.com>
2018-05-10 13:53:18 +0100

    made some housekeeping in app.js

--- controllers/request.js
+++ controllers/request.js
@@ -2,11 +2,11 @@
 
 const request = require('request');
 
-const api_options = { server: 'http://127.0.0.1:5000' };
+const api_options = { server: 'http://127.0.0.1:3000' };
 const render = (page, options, req, res) => res.render(page, options);
 
 if (process.env.NODE_ENV === 'production') {
-    api_options.server = 'https://sleepy-mesa-12188.herokuapp.com/';
+    api_options.server = 'https://sleepy-mesa-12188.herokuapp.com';
 }
 
 exports.render = render;
