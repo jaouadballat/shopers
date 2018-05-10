a199e9d68d499265464d587a7f96e6e4655bda41
Muhammad Tajjiou <shutsugan@gmail.com>
2018-05-10 12:04:42 +0100

    some housekeeping in package.json

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(flash());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('x-powered-by', false);
+//app.set('x-powered-by', false);
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
